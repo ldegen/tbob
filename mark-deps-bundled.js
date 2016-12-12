73e89f6d6c0fcf380354467ff0371a8c26cbd9f4
Lukas Degener <l.degener@tarent.de>
2016-12-12 13:06:16 +0100

    remove mark-depes-bundled.js hack

--- mark-deps-bundled.js
+++
@@ -1,5 +0,0 @@
-var fs = require("fs");
-var path = require("path");
-var p = require("./package.json");
-p.bundleDependencies = Object.keys(p.dependencies);
-fs.writeFileSync(path.join(__dirname, 'package.json'), JSON.stringify(p, null, '  '));
